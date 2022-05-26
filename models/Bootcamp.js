c70ed7d57ebde18e03df8bea24c381aaa7045be6
Vighnesh Manjrekar <vighneshmanjrekar4193@gmail.com>
2022-05-27 01:00:05 +0530

    Add geojson

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
 
+const geocoder = require("../middleware/geocode");
+
 const BootcampSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -101,6 +103,21 @@ const BootcampSchema = new mongoose.Schema({
 
 BootcampSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+BootcampSchema.pre("save", async function (next) {
+  const [loc] = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc.longitude, loc.latitude],
+    formattedAddress: loc.formattedAddress,
+    street: loc.streetName,
+    city: loc.city,
+    state: loc.stateCode,
+    zipcode: loc.zipcode,
+    country: loc.countryCode,
+  };
   next();
 });
 
